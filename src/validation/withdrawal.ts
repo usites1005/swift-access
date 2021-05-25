6973554b33a540d7dae4be8c1e7acd9ea62efc36
Uniqcoda <etokwudomary@gmail.com>
2021-05-25 22:09:10 +0100

    add validation for createUserAccount

--- src/validation/withdrawal.ts
+++ src/validation/withdrawal.ts
@@ -1,15 +1,5 @@
 import { Joi, Segments } from 'celebrate';
 
-export const deposit = {
-  [Segments.BODY]: Joi.object().keys({
-		userId: Joi.string().required(),
-		dollarAmount: Joi.string().required(),
-		destination: Joi.string().required(),
-		sender: Joi.string().required(),
-		comment: Joi.string(),
-	}),
-}
-
 export const withdrawal = {
   [Segments.BODY]: Joi.object().keys({
 		userId: Joi.string().required(),
